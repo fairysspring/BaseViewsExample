851c676562c4c08c7f0bd3864bb8e5c618b65ba4
fairysspring <hisonglixian@qq.com>
2014-10-22 23:21:49 +0800

    test unbutton and uilabel for si’zeToFit

--- BaseViewsExample/BaseViewsExample/ViewController.m
+++ BaseViewsExample/BaseViewsExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIViewAdditions.h"
 
 @interface ViewController ()
 
@@ -17,7 +18,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self testButton_sizetofit];
+    [self testLabel_sizetofit];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)testLabel_sizetofit
+{
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.font = [UIFont systemFontOfSize:12];
+    label.text = @"ç√√√√NSLineBreakByCharWrappingNSLineBreakByCharWrapping";
+    
+    label.top = 100;
+    label.width = 100;//设置宽度，来使sizetofit 控制在此宽度，两行内 或者 不设置宽度，使其展示在一行内。
+    label.numberOfLines = 2;
+    label.lineBreakMode = NSLineBreakByCharWrapping;
+    [label sizeToFit];
+    label.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:label];
+}
+
+- (void)testButton_sizetofit
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.titleLabel.font = [UIFont systemFontOfSize:12.0f	];
+    button.top = 200;
+    button.backgroundColor = [UIColor blackColor];
+    [button setTitle:@"点击我试试" forState:UIControlStateNormal];
+    
+    //设置button的各个边距
+    button.contentEdgeInsets = UIEdgeInsetsMake(30, 30, 30, 30);
+    [button sizeToFit];
+    [self.view addSubview:button];
+    
 }
 
 - (void)didReceiveMemoryWarning
